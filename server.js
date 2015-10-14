afa54e1cb21ebaaa976f92e448d9694101726ba6
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-15 01:55:51 +0300

    wut3

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 
 
 var express  = require('express');
+var pg       = require('pg');
 var app      = express();
 
 var port = process.env.PORT || 8080;
 
-var knex = require('knex')({
-	client: 'pg',
-	connection: process.env.DATABASE_URL
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
 });
-
-knex.schema.hasTable('users').then(function(exists) {
-	if (!exists) {
-		return knex.schema.createTable('users', function(t) {
-			t.increments('id').primary();
-			t.string('nimi', 100);
-		});
-	}
-});
-
-knex('users').insert({nimi: 'antti'})
-
-app.listen(port);
-console.log("App listening on port " + port);
-
