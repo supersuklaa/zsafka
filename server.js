6320468d5664444c275c420cc53397d52ec769ff
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-15 02:01:13 +0300

    wut

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var express  = require('express');
 var pg       = require('pg');
 var app      = express();
 
-var port = process.env.PORT || 8080;
+//var port = process.env.PORT || 8080;
 
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-
+/*
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
-    });
+    });*/
 });
