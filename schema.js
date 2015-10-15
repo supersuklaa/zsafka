5547af08d98c016781217c0be75965301072db56
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-15 22:37:02 +0300

    cleaning

--- schema.js
+++ schema.js
@@ -1,73 +1,45 @@
-/*
+
 var config      = require('./config');
 var knex        = require('knex')(config.db);
-*/
-var db = {};
 
-db.create = function (knex) {
+var createTables = function() {
 
-	console.log("jeejee");
+	knex.schema.hasTable('users').then(function(exists) {
+		if (!exists) {
+			return knex.schema.createTable('users', function(t) {
+				t.increments('id').primary();
+				t.string('nimi', 100);
+				t.string('password', 100);
+			});
+		}
+	});
 
-	return knex.schema.
-		createTable('users', function(t) {
-			t.increments('id').primary();
-			t.string('nimi', 100);
-			t.string('password', 100);
-		}).
-		createTable('safkat', function(t) {
-			t.increments('id').primary();
-			t.string('nimi', 100);
-			t.float('rasva');
-			t.float('hiilari');
-			t.float('protsku');
-		}).
-		createTable('annokset', function(t) {
-			t.increments('id').primary();
-			t.dateTime('date');
-			t.integer('maara');
-			t.integer('safka').references('id').inTable('safkat');
-			t.integer('user').references('id').inTable('users');
-		});
+	knex.schema.hasTable('safkat').then(function(exists) {
+		if (!exists) {
+			return knex.schema.createTable('safkat', function(t) {
+				t.increments('id').primary();
+				t.string('nimi', 100);
+				t.float('rasva').defaulTo(0);
+				t.float('hiilari').defaulTo(0);
+				t.float('protsku').defaulTo(0);
+			});
+		}
+	});
 
-/*		
-	return knex.schema.
-		hasTable('users').then(function(exists) {
-			if (!exists) {
-				return knex.schema.createTable('users', function(t) {
-					t.increments('id').primary();
-					t.string('nimi', 100);
-					t.string('password', 100);
-				});
-			}
-		}).
+	knex.schema.hasTable('annokset').then(function(exists) {
+		if (!exists) {
+			return knex.schema.createTable('annokset', function(t) {
+				t.increments('id').primary();
+				t.dateTime('date');
+				t.integer('maara');
+				t.integer('safka').references('id').inTable('safkat');
+				t.integer('user').references('id').inTable('users');
+			});
+		}
+	});
 
-		hasTable('safkat').then(function(exists) {
-			if (!exists) {
-				return knex.schema.createTable('safkat', function(t) {
-					t.increments('id').primary();
-					t.string('nimi', 100);
-					t.float('rasva');
-					t.float('hiilari');
-					t.float('protsku');
-				});
-			}
-		}).
-
-		hasTable('annokset').then(function(exists) {
-			if (!exists) {
-				return knex.schema.createTable('annokset', function(t) {
-					t.increments('id').primary();
-					t.dateTime('date');
-					t.integer('maara');
-					t.integer('safka').references('id').inTable('safkat');
-					t.integer('user').references('id').inTable('users');
-				});
-			}
-		});
-*/
 }
 
-// Module exports
-module.exports = {
-	create: db.create
+module.exports {
+	createTables: createTables
 }
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 
 var express  = require('express');
+var bodyParser = require('body-parser'); 
 
-var config   = require('./config');
-var knex     = require('knex')(config.db);
 var schema   = require('./schema');
 
 var app      = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); 
+
 var port = process.env.PORT || 8080;
 
-schema.create(knex);
+// Routes
 
+app.get('/createTables', function(req, res) {
+	schema.createTables();
+});
 
 app.listen(port);
 console.log("App listening on port " + port);
