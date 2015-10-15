389771c0aafe3e8107e320a94f8680219640fbee
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-15 11:07:40 +0300

    jokoo

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var express  = require('express');
 var pg       = require('pg');
 var app      = express();
 
-//var port = process.env.PORT || 8080;
-
+var port = process.env.PORT || 8080;
+/*
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+*/
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
@@ -19,3 +26,6 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
         res.send('jee'); 
     });
 });
+
+app.listen(port);
+console.log("App listening on port " + port);
