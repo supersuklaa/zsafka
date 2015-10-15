5d2b583100e08d81afab0ea80e339622fb9ecb34
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-15 11:27:17 +0300

    knexitest

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 
 
 var express  = require('express');
-var pg       = require('pg');
 var app      = express();
+var pg       = require('pg');
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+});
 
 var port = process.env.PORT || 8080;
-/*
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
-*/
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-/*
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });*/
-    app.get('*', function(req, res) {
-        res.send('jee'); 
+
+knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    return knex.schema.createTable('users', function(t) {
+      t.increments('id').primary();
+      t.string('first_name', 100);
+      t.string('last_name', 100);
+      t.text('bio');
     });
+  }
 });
 
 app.listen(port);
