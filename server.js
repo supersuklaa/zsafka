24d925685d2990252b80f0324e7d82fbd1e58345
supersuklaa <antti.sartjarvi@gmail.com>
2015-10-16 02:19:50 +0300

    jea angu

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var bodyParser   = require('body-parser');
 var schema       = require('./schema');
 var users        = require('./users');
 
-
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
